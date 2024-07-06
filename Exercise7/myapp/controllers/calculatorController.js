624f9caabc5c0f365c80c23aff3c425442882356
KerryPower <kerrycpower@gmail.com>
2024-07-06 14:53:58 +1200

    Exercise 7 parts 1 &2

---
+++ Exercise7/myapp/controllers/calculatorController.js
@@ -0,0 +1,41 @@
+const Calculator = require('../libraries/Calculator');
+let myCalc = new Calculator()
+
+const addNumbers = ('/add', (req, res) => {
+    let number1 = parseInt(req.query.num1);
+    let number2 = parseInt(req.query.num2);
+    let sum = myCalc.add(number1,number2)
+    console.log(sum);
+    res.status(200);
+    res.json({ result: sum })
+})
+const multiplyNumbers = ('/multiply', (req, res) => {
+    let number1 = parseInt(req.query.num1);
+    let number2 = parseInt(req.query.num2);
+    let multiple = myCalc.multiply(number1,number2)
+    console.log(multiple);
+    res.status(200);
+    res.json({ result: multiple })
+})
+const subtractNumbers = ('/subtract', (req, res) => {
+    let number1 = parseInt(req.query.num1);
+    let number2 = parseInt(req.query.num2);
+    let subtraction = myCalc.subtract(number1,number2)
+    console.log(subtraction);
+    res.status(200);
+    res.json({ result: subtraction })
+})
+const divideNumbers = ('/divide', (req, res) => {
+    let number1 = parseInt(req.query.num1);
+    let number2 = parseInt(req.query.num2);
+    let division = myCalc.divide(number1,number2)
+    console.log(division);
+    res.status(200);
+    res.json({ result: division })
+})
+module.exports = {
+    addNumbers,
+    subtractNumbers,
+    multiplyNumbers,
+    divideNumbers
+}
